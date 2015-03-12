bbef17dee7b8706ad82028817586058ea68822a2
ambassador86 <ambassador86@gmail.com>
2015-03-12 23:03:26 +0100

    ordered output for better view

--- index.php
+++ index.php
@@ -1,30 +1,29 @@
 <?php
-
-$fh=fopen("stats.conf", "r");
+$fh = fopen ( "stats.conf", "r" );
 
 if ($fh) {
-    while (($buffer = fgets($fh, 4096)) !== false) {
-    	$buffer = str_replace("\"", "", $buffer);
-        $splitted_line  = explode("=", $buffer);
+	while ( ($buffer = fgets ( $fh, 4096 )) !== false ) {
+		$buffer = str_replace ( "\"", "", $buffer );
+		$splitted_line = explode ( "=", $buffer );
 
-        if($splitted_line[0] == "interfaces"){
-        	$interfaces = explode(" ", $splitted_line[1]);
-        }
-        if($splitted_line[0] == "outputs"){
-        	$outputs = explode(" ", $splitted_line[1]);
-        }
-    }
-    fclose($fh);
+		if ($splitted_line [0] == "interfaces") {
+			$interfaces = explode ( " ", $splitted_line [1] );
+		}
+		if ($splitted_line [0] == "outputs") {
+			$outputs = explode ( " ", $splitted_line [1] );
+		}
+	}
+	fclose ( $fh );
 }
 
 echo "<html><body><h1>Traffic Statistiken für ";
-echo gethostname();
+echo gethostname ();
 echo "</h1>";
 echo "<table>";
-foreach ($interfaces as $iface){
+foreach ( $outputs as $mode ) {
 	echo "<tr>";
-	foreach ($outputs as $mode){
-		echo "<td><img src=\"".$iface."_".$mode.".png\" /></td>";
+	foreach ( $interfaces as $iface ) {
+		echo "<td><img src=\"" . $iface . "_" . $mode . ".png\" /></td>";
 	}
 	echo "</tr>";
 }
