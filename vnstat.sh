53b2b11a45136fa5a7ddeb5430f5284540b4f247
ambassador86 <ambassador86@gmail.com>
2015-03-12 22:59:05 +0100

    add update per interface to enable new interfaces

--- vnstat.sh
+++ vnstat.sh
@@ -3,6 +3,7 @@ vnstat -u
 source stats.conf
 for interface in $interfaces; do
 for output in $outputs; do
+  vnstat -u -i ${interface}
   vnstati -${output} -i ${interface} -o ${wwwroot}/${interface}_${output}.png
 done
 done
